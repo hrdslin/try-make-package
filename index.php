5da95c83fad51fb4d016520ffd674e4798ab4487
lin_ming_chang <hrds@foxmail.com>
2020-09-11 10:30:49 +0800

    push

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @datetime  2020-09-11 10:24
+ * @author    LMC
+ */
+require_once 'vendor/autoload.php';
+
+new \Flower\Rose\TestClass();
---
+++ src/Flower/Rose/TestClass.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @datetime  2020-09-11 10:23
+ * @author    LMC
+ */
+
+namespace Flower\Rose;
+
+
+class TestClass
+{
+
+    public function __construct()
+    {
+        echo 112233;
+    }
+
+}
